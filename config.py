9ebcce98813e98c869660d3faf5350c9fe3e4fe5
Igor Vassilyev <igor52677@gmail.com>
2025-08-18 12:35:37 +0500

    add config

---
+++ config.py
@@ -0,0 +1,10 @@
+CURRENT_IMAGE_SIZE = (4032, 3024)  # (ширина, высота) в пикселях
+
+# # Настройки обработки
+# MIN_CAMERAS_PER_MARKER = 3      # минимум камер для триангуляции
+# MAX_REPROJECTION_ERROR = 2.0    # максимальная ошибка в пикселях
+
+# # Пути к файлам
+# XMP_DIRECTORY = "data"           # папка с XMP файлами
+# IMAGES_DIRECTORY = "images"      # папка с изображениями
+# OUTPUT_BLENDER = "blender_scene.json"  # файл для Blender
